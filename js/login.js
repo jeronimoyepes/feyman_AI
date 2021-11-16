496b6e5ed2bb21cd02d177fcc7f64f3e7fc4ee80
jeronimoyepes <jeronimo_y_g@hotmail.com>
2021-11-16 02:49:30 -0500

    Primer Commit

---
+++ js/login.js
@@ -0,0 +1,43 @@
+var idCodeInput = document.querySelector("#id_code")
+
+idCodeInput.focus();
+
+
+
+// Certificación de código de entrada
+
+function accessApp(e){
+    var keynum;
+
+    
+    // idCodeInput.value = "/" + idCodeInput.value;
+    
+    // Encontrar la tecla presionada
+    if(window.event) { // IE                  
+        keynum = e.keyCode;
+    } else if(e.which){ // Netscape/Firefox/Opera                 
+        keynum = e.which;
+    }
+    console.log(keynum)
+    // Si es la tecla [Enter] redirigir o mostrar mensaje "incorrecto"
+    if (keynum == 13) {
+        if (idCodeInput.value == "110591-8") {
+            window.location.href = "video.html";
+        } else {
+            idCodeInput.value = "";
+            idCodeInput.setAttribute('placeholder', 'Código incorrecto')
+            setTimeout(function () {
+                idCodeInput.setAttribute('placeholder', '')
+            }, 600);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
